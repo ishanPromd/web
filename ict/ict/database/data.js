2abea1296b1c31ea7984031784933dca3f5250f9
ishanPromd <ishanstc123@gmail.com>
2025-07-05 15:56:53 +0530

    Add files via upload

---
+++ ict/ict/database/data.js
@@ -0,0 +1,122 @@
+const lessonData = {
+    '1': {
+        videoSrc: "https://www.youtube.com/embed/dCdj9qxxXkE?controls=0&showinfo=0&rel=0&modestbranding=1&iv_load_policy=3&disablekb=1&playsinline=1", 
+        title: "Database (day 01)",
+        teacher: "Ravindu Bandaranayake",
+        teacherImg: "https://yt3.googleusercontent.com/bRzjH5rIbDRaHCiXopbsLJh8w64sKd5QduITMma3GZZd6e5Yr1Jp1uaJlHvNN4E37sMiJNTj0A=s900-c-k-c0x00ffffff-no-rj",
+        prevUrl: "/index.html", 
+        prevText: "Back To Database",
+        nextUrl: "watch-video?id=2",
+        nextText: "Next"
+    },
+    '2': {
+        videoSrc: "https://www.youtube.com/embed/_DGtqByNhfk?controls=0&showinfo=0&rel=0&modestbranding=1&iv_load_policy=3&disablekb=1&playsinline=1", 
+        title: "Database (day 02)",
+        teacher: "Ravindu Bandaranayake",
+        teacherImg: "https://yt3.googleusercontent.com/bRzjH5rIbDRaHCiXopbsLJh8w64sKd5QduITMma3GZZd6e5Yr1Jp1uaJlHvNN4E37sMiJNTj0A=s900-c-k-c0x00ffffff-no-rj",
+        prevUrl: "watch-video?id=1",
+        prevText: "Previous",
+        nextUrl: "watch-video?id=3",
+        nextText: "Next"
+    },
+    '3': {
+        videoSrc: "https://www.youtube.com/embed/DLhsbVEhqQw?controls=0&showinfo=0&rel=0&modestbranding=1&iv_load_policy=3&disablekb=1&playsinline=1",
+        title: "Database (day 03)",
+        teacher: "Ravindu Bandaranayake",
+        teacherImg: "https://yt3.googleusercontent.com/bRzjH5rIbDRaHCiXopbsLJh8w64sKd5QduITMma3GZZd6e5Yr1Jp1uaJlHvNN4E37sMiJNTj0A=s900-c-k-c0x00ffffff-no-rj",
+        prevUrl: "watch-video?id=2",
+        prevText: "Previous",
+        nextUrl: "watch-video?id=4",
+        nextText: "Next"
+    },
+    '4': {
+        videoSrc: "https://www.youtube.com/embed/U4f47fFf5qw?controls=0&showinfo=0&rel=0&modestbranding=1&iv_load_policy=3&disablekb=1&playsinline=1",
+        title: "Database (Day 04)",
+        teacher: "Ravindu Bandaranayake",
+        teacherImg: "https://yt3.googleusercontent.com/bRzjH5rIbDRaHCiXopbsLJh8w64sKd5QduITMma3GZZd6e5Yr1Jp1uaJlHvNN4E37sMiJNTj0A=s900-c-k-c0x00ffffff-no-rj",
+        prevUrl: "watch-video?id=3",
+        prevText: "Previous",
+        nextUrl: "watch-video?id=5",
+        nextText: "Next"
+    },
+    '5': {
+        videoSrc: "https://www.youtube.com/embed/LHfpBB16_7A?controls=0&showinfo=0&rel=0&modestbranding=1&iv_load_policy=3&disablekb=1&playsinline=1",
+        title: "Database (Day 05)",
+        teacher: "Ravindu Bandaranayake",
+        teacherImg: "https://yt3.googleusercontent.com/bRzjH5rIbDRaHCiXopbsLJh8w64sKd5QduITMma3GZZd6e5Yr1Jp1uaJlHvNN4E37sMiJNTj0A=s900-c-k-c0x00ffffff-no-rj",
+        prevUrl: "watch-video?id=4",
+        prevText: "Previous",
+        nextUrl: "watch-video?id=6",
+        nextText: "Next"
+    },
+    '6': {
+        videoSrc: "https://www.youtube.com/embed/f4O1P0uRBxg?controls=0&showinfo=0&rel=0&modestbranding=1&iv_load_policy=3&disablekb=1&playsinline=1",
+        title: "Database - Theory - 2025 AL (Er digrams)",
+        teacher: "Ravindu Bandaranayake",
+        teacherImg: "https://yt3.googleusercontent.com/bRzjH5rIbDRaHCiXopbsLJh8w64sKd5QduITMma3GZZd6e5Yr1Jp1uaJlHvNN4E37sMiJNTj0A=s900-c-k-c0x00ffffff-no-rj",
+        prevUrl: "watch-video?id=5",
+        prevText: "Previous",
+        nextUrl: "watch-video?id=7",
+        nextText: "Next"
+    },
+    '7': {
+        videoSrc: "https://www.youtube.com/embed/xInxfcYAjAM?controls=0&showinfo=0&rel=0&modestbranding=1&iv_load_policy=3&disablekb=1&playsinline=1",
+        title: "Database (Day 06)",
+        teacher: "Ravindu Bandaranayake",
+        teacherImg: "https://yt3.googleusercontent.com/bRzjH5rIbDRaHCiXopbsLJh8w64sKd5QduITMma3GZZd6e5Yr1Jp1uaJlHvNN4E37sMiJNTj0A=s900-c-k-c0x00ffffff-no-rj",
+        prevUrl: "watch-video?id=6",
+        prevText: "Previous",
+        nextUrl: "watch-video?id=8",
+        nextText: "Next"
+    },
+    '8': {
+        videoSrc: "https://www.youtube.com/embed/9MI880xLeh4?controls=0&showinfo=0&rel=0&modestbranding=1&iv_load_policy=3&disablekb=1&playsinline=1",
+        title: "Database (Day 07)",
+        teacher: "Ravindu Bandaranayake",
+        teacherImg: "https://yt3.googleusercontent.com/bRzjH5rIbDRaHCiXopbsLJh8w64sKd5QduITMma3GZZd6e5Yr1Jp1uaJlHvNN4E37sMiJNTj0A=s900-c-k-c0x00ffffff-no-rj",
+        prevUrl: "watch-video?id=7",
+        prevText: "Previous",
+        nextUrl: "watch-video?id=9",
+        nextText: "Next"
+    },
+    '9': {
+        videoSrc: "https://www.youtube.com/embed/4u-fhDQxISM?controls=0&showinfo=0&rel=0&modestbranding=1&iv_load_policy=3&disablekb=1&playsinline=1",
+        title: "Database - Theory - 2025 AL (Bubble Sort)",
+        teacher: "Ravindu Bandaranayake",
+        teacherImg: "https://yt3.googleusercontent.com/bRzjH5rIbDRaHCiXopbsLJh8w64sKd5QduITMma3GZZd6e5Yr1Jp1uaJlHvNN4E37sMiJNTj0A=s900-c-k-c0x00ffffff-no-rj",
+        prevUrl: "watch-video?id=8",
+        prevText: "Previous",
+        nextUrl: "watch-video?id=10",
+        nextText: "Next"
+    },
+    '10': {
+        videoSrc: "https://www.youtube.com/embed/vKdKx55s95E?controls=0&showinfo=0&rel=0&modestbranding=1&iv_load_policy=3&disablekb=1&playsinline=1",
+        title: "Database - Theory - 2025 AL (Bubble Sort Continue)",
+        teacher: "Ravindu Bandaranayake",
+        teacherImg: "https://yt3.googleusercontent.com/bRzjH5rIbDRaHCiXopbsLJh8w64sKd5QduITMma3GZZd6e5Yr1Jp1uaJlHvNN4E37sMiJNTj0A=s900-c-k-c0x00ffffff-no-rj",
+        prevUrl: "watch-video?id=9",
+        prevText: "Previous",
+        nextUrl: "watch-video?id=11",
+        nextText: "Next"
+    },
+    '11': {
+        videoSrc: "https://www.youtube.com/embed/RL9WvFNq_QY?controls=0&showinfo=0&rel=0&modestbranding=1&iv_load_policy=3&disablekb=1&playsinline=1",
+        title: "Database - Theory - 2025 AL (Database Normalization Day 1)",
+        teacher: "Ravindu Bandaranayake",
+        teacherImg: "https://yt3.googleusercontent.com/bRzjH5rIbDRaHCiXopbsLJh8w64sKd5QduITMma3GZZd6e5Yr1Jp1uaJlHvNN4E37sMiJNTj0A=s900-c-k-c0x00ffffff-no-rj",
+        prevUrl: "watch-video?id=10",
+        prevText: "Previous",
+        nextUrl: "watch-video?id=12",
+        nextText: "Next"
+    },
+    '12': {
+        videoSrc: "https://www.youtube.com/embed/e58WGK-th6M?controls=0&showinfo=0&rel=0&modestbranding=1&iv_load_policy=3&disablekb=1&playsinline=1",
+        title: "Database - Theory - 2025 AL (Database Normalization Day 2)",
+        teacher: "Ravindu Bandaranayake",
+        teacherImg: "https://yt3.googleusercontent.com/bRzjH5rIbDRaHCiXopbsLJh8w64sKd5QduITMma3GZZd6e5Yr1Jp1uaJlHvNN4E37sMiJNTj0A=s900-c-k-c0x00ffffff-no-rj",
+        prevUrl: "watch-video?id=11",
+        prevText: "Previous",
+        nextUrl: "/paper/sft/playlist.html",
+        nextText: "Back to Playlist"
+    }
+};
---
+++ ict/ict/database/index.js
@@ -0,0 +1,95 @@
+// Data for the specific Database lessons
+const civilLessonsData = [
+    // Week 1
+    {
+        imgSrc: "https://t4.ftcdn.net/jpg/05/27/56/59/360_F_527565942_mM357p32HN1fPVGFI51f2vZNsXQAMEd0.jpg",
+        title: "Database - Theory - 2025 AL (Day 01)",
+        subtitle: "1st Week - 2025 AL\nPhysical Lesson Pack",
+        duration: "3:26:46",
+        url: "watch-video?id=1"
+    },
+    {
+        imgSrc: "https://t4.ftcdn.net/jpg/05/27/56/59/360_F_527565942_mM357p32HN1fPVGFI51f2vZNsXQAMEd0.jpg",
+        title: "Database - Theory - 2025 AL (Day 02)",
+        subtitle: "1st Week - 2025 AL\nPhysical Lesson Pack",
+        duration: "3:37:52",
+        url: "watch-video?id=2"
+    },
+    
+    // Week 2
+    {
+        imgSrc: "https://t4.ftcdn.net/jpg/05/27/56/59/360_F_527565942_mM357p32HN1fPVGFI51f2vZNsXQAMEd0.jpg",
+        title: "Database - Theory - 2025 AL (Day 03)",
+        subtitle: "2nd Week - 2025 AL\nPhysical Lesson Pack",
+        duration: "3:26:55",
+        url: "watch-video?id=3"
+    },
+    {
+        imgSrc: "https://t4.ftcdn.net/jpg/05/27/56/59/360_F_527565942_mM357p32HN1fPVGFI51f2vZNsXQAMEd0.jpg",
+        title: "Database - Theory - 2025 AL (Day 04)",
+        subtitle: "2nd Week - 2025 AL\nPhysical Lesson Pack",
+        duration: "3:22:17",
+        url: "watch-video?id=4"
+    },
+    {
+        imgSrc: "https://t4.ftcdn.net/jpg/05/27/56/59/360_F_527565942_mM357p32HN1fPVGFI51f2vZNsXQAMEd0.jpg",
+        title: "Database - Theory - 2025 AL (Day 05)",
+        subtitle: "2nd Week - 2025 AL\nPhysical Lesson Pack",
+        duration: "1:15:28",
+        url: "watch-video?id=5"
+    },
+    
+    // Week 3
+    {
+        imgSrc: "https://t4.ftcdn.net/jpg/05/27/56/59/360_F_527565942_mM357p32HN1fPVGFI51f2vZNsXQAMEd0.jpg",
+        title: "Database - Theory - 2025 AL (Er digrams)",
+        subtitle: "3rd Week - 2025 AL\nPhysical Lesson Pack",
+        duration: "3:31:40",
+        url: "watch-video?id=6"
+    },
+    {
+        imgSrc: "https://t4.ftcdn.net/jpg/05/27/56/59/360_F_527565942_mM357p32HN1fPVGFI51f2vZNsXQAMEd0.jpg",
+        title: "Database - Theory - 2025 AL (Day 06)",
+        subtitle: "3rd Week - 2025 AL\nPhysical Lesson Pack",
+        duration: "3:18:22",
+        url: "watch-video?id=7"
+    },
+    {
+        imgSrc: "https://t4.ftcdn.net/jpg/05/27/56/59/360_F_527565942_mM357p32HN1fPVGFI51f2vZNsXQAMEd0.jpg",
+        title: "Database - Theory - 2025 AL (Day 07)",
+        subtitle: "3rd Week - 2025 AL\nPhysical Lesson Pack",
+        duration: "1:45:33",
+        url: "watch-video?id=8"
+    },
+    
+    // Week 4
+    {
+        imgSrc: "https://t4.ftcdn.net/jpg/05/27/56/59/360_F_527565942_mM357p32HN1fPVGFI51f2vZNsXQAMEd0.jpg",
+        title: "Database - Theory - 2025 AL (Bubble Sort)",
+        subtitle: "4th Week - 2025 AL\nPhysical Lesson Pack",
+        duration: "3:29:15",
+        url: "watch-video?id=9"
+    },
+    {
+        imgSrc: "https://t4.ftcdn.net/jpg/05/27/56/59/360_F_527565942_mM357p32HN1fPVGFI51f2vZNsXQAMEd0.jpg",
+        title: "Database - Theory - 2025 AL (Bubble Sort Continue)",
+        subtitle: "4th Week - 2025 AL\nPhysical Lesson Pack",
+        duration: "3:34:08",
+        url: "watch-video?id=10"
+    },
+    {
+        imgSrc: "https://t4.ftcdn.net/jpg/05/27/56/59/360_F_527565942_mM357p32HN1fPVGFI51f2vZNsXQAMEd0.jpg",
+        title: "Database - Theory - 2025 AL (Database Normalization Day 1)",
+        subtitle: "4th Week - 2025 AL\nPhysical Lesson Pack",
+        duration: "1:28:47",
+        url: "watch-video?id=11"
+    }
+,
+    {
+        imgSrc: "https://t4.ftcdn.net/jpg/05/27/56/59/360_F_527565942_mM357p32HN1fPVGFI51f2vZNsXQAMEd0.jpg",
+        title: "Database - Theory - 2025 AL (Database Normalization Day 2)",
+        subtitle: "4th Week - 2025 AL\nPhysical Lesson Pack",
+        duration: "1:28:47",
+        url: "watch-video?id=12"
+    }
+];
